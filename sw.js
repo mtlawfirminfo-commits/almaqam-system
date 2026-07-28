c550817eec5c1ca0f4694ffcf949fda8e2099cf6
mtlawfirminfo-commits <mtlawfirminfo@gmail.com>
2026-07-28 18:34:54 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'almaqam-v1';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
